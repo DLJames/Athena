17e3546ea8804b91b101afbae7ff72a3d8446ae8
Bao Nan Shan(James) <sbaonan@cn.ibm.com>
2019-10-19 10:32:13 +0800

    update home page

--- UI/athena/src/App.js
+++ UI/athena/src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import BScroll from 'better-scroll';
+
+import './app.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.initScroll();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>This is a new Athena project~</h1>
-        </header>
+      <div className="athena-app">
+        <Header></Header>
+        <div className="athena-wraper wrapper" ref={(scroll) => {this.scrollView = scroll}}>
+          <div className="athena-content">
+            {new Array(80).fill(1).map((item,index)=>{
+              return <div key={index}>content- {index}</div>
+            })}
+          </div>
+        </div>
       </div>
     );
+  }
+
+  initScroll() {
+    this.scroll = new BScroll(this.scrollView, {
+      mouseWheel: {
+        speed: 20,
+        invert: false,
+        easeTime: 300
+      },
+      scrollbar: {
+        fade: false,
+        interactive: false
+      }
+    });
+  }
+
+  resize () {
+    this.scroll.refresh();
   }
 }
 
---
+++ UI/athena/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="athena-header">Header</div>
+        );
+    }
+}
+
+export default Header;
