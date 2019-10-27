2f7e2e5484a14d24da8a9e371c6b22da0f86b38d
Bao Nan Shan(James) <sbaonan@cn.ibm.com>
2019-10-27 12:46:41 +0800

    update

--- UI/athena/src/pages/home/index.js
+++ UI/athena/src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../../components/SearchBar';
+import ContactItem from './components/ContactItem';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 import './style.css';
@@ -36,6 +37,8 @@ class Home extends Component {
         }
       ]
     }
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount(){
@@ -55,20 +58,20 @@ class Home extends Component {
     const contactNum = this.state.contactList.length;
     const contactName = this.state.contactName;
     return (
-      <div className="athena-home" ref={(domNode) => {this.domNode = domNode}}>
-        <div className="athena-home-search" ref={(domNode) => {this.searchNode = domNode}}>
+      <div className="athena-home">
+        <div className="athena-home-search">
           <SearchBar focus={false} searchVal={this.state.contactName}></SearchBar>
         </div>
-        <div className="athena-home-info" ref={(domNode) => {this.infoNode = domNode}}>
+        <div className="athena-home-info">
           <div>Showing {contactNum} results for <span className="athena-home-contact">{contactName}</span></div>
           <div>We've included results that are incomplete and possibly out of date.</div>
           <div className="athena-home-showFour"><span>Show 4 results instead</span></div>
         </div>
-        <div className="athena-home-contactCon" ref={(domNode) => {this.contactNode = domNode}}>
+        <div className="athena-home-contactCon">
           <Scrollbars>
-            <div>
-              {new Array(80).fill(1).map((item,index)=>{
-                return <div key={index}>content- {index}</div>
+            <div className="athena-home-contactItemCon">
+              {this.state.contactList.map((item)=>{
+                return <ContactItem key={item.id} contactData={item}></ContactItem>
               })}
             </div>
           </Scrollbars>
